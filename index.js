4bf9671fa8572c88731255ee3fabc9b026ed9994
yukino-kikusaki <yukino.kikusaki@azone-japan.co.jp>
2024-04-18 11:49:29 +0900

    解答済み

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 'use strict';
 const http = require('node:http');
-const cp = require('node:child_process');
 const server = http.createServer((req, res) => {
   const path = req.url;
   res.writeHead(200, {
     'Content-Type': 'text/plain; charset=utf-8'
   });
-  res.end(cp.execSync(`echo ${path}`));
+  res.end(path);
 });
 const port = 8000;
 server.listen(port, () => {
